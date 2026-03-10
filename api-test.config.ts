b2451e3e6390264d7e106bffff26ff5d491a6f57
Dan <danjewellesanchez+github@gmail.com>
2026-03-11 07:45:44 +0800

    chore: first commit

---
+++ api-test.config.ts
@@ -0,0 +1,18 @@
+const processENV = process.env.TEST_ENV;
+const env = processENV || "dev";
+console.log("Test environment is: " + env);
+
+const config = {
+  apiUrl: "https://jsonplaceholder.typicode.com",
+  userEmail: "templateapiuser@xyz.com",
+  userPassword: "Welcome",
+};
+
+if (env === "qa") {
+  ((config.userEmail = ""), (config.userPassword = ""));
+}
+if (env === "prod") {
+  ((config.userEmail = ""), (config.userPassword = ""));
+}
+
+export { config };
---
+++ tests/smokeTest.spec.ts
@@ -0,0 +1,52 @@
+import { test } from "../utils/fixtures";
+import { expect } from "../utils/custom-expect";
+
+test("GET @regression @smoke", async ({ publicApi }) => {
+  const apiResponse = await publicApi
+    .path("/posts/1")
+    .clearAuth()
+    .getRequest(200);
+  await expect(apiResponse).shouldMatchSchema("posts", "GET_posts");
+  expect(apiResponse.title).shouldEqual(
+    "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+  );
+});
+
+// test("Create and Delete Article", async ({ api }) => {
+//   const createArticleResponse = await api
+//     .path("/articles")
+//     .body({
+//       article: {
+//         title: "Hello World",
+//         description: "Hello World",
+//         body: "HELLO",
+//         tagList: [],
+//       },
+//     })
+//     .postRequest(201);
+//   await expect(createArticleResponse).shouldMatchSchema(
+//     "articles",
+//     "POST_articles",
+//   );
+//   expect(createArticleResponse.article.title).shouldEqual("Hello World");
+//   const slugId = createArticleResponse.article.slug;
+
+//   const articlesResponse = await api
+//     .path("/articles")
+//     .params({ limit: 10, offset: 0 })
+//     .getRequest(200);
+//   await expect(articlesResponse).shouldMatchSchema("articles", "GET_articles");
+//   expect(articlesResponse.articles[0].title).shouldEqual("Hello World");
+
+//   await api.path(`/articles/${slugId}`).deleteRequest(204);
+
+//   const articlesResponseTwo = await api
+//     .path("/articles")
+//     .params({ limit: 10, offset: 0 })
+//     .getRequest(200);
+//   await expect(articlesResponseTwo).shouldMatchSchema(
+//     "articles",
+//     "GET_articles",
+//   );
+//   expect(articlesResponseTwo.articles[0].title).not.shouldEqual("Hello World");
+// });
